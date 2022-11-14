43f6d7d2d8b1f327613600e7830c2a324b043494
Heveraldo <heveraldob12@gmail.com>
2022-11-14 09:56:00 -0300

    first commit

---
+++ src/app/main.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+from flask_basicauth import BasicAuth
+from textblob import TextBlob
+from sklearn.linear_model import LinearRegression
+import pickle
+
+modelo = pickle.load(open('modelo.sav','rb'))
+colunas = ['tamanho','ano','garagem']
+
+
+
+
+app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = 'Heveraldo API'
+app.config['BASIC_AUTH_PASSWORD'] = 'aluracurso'
+
+basic_auth = BasicAuth(app)
+
+@app.route('/')
+def home():
+    return "Minha primeira API."
+
+@app.route('/sentimento/<frase>')
+@basic_auth.required
+def sentimento(frase):
+    tb = TextBlob(frase)
+    tb_en = tb.translate(from_lang='pt_br',to='en')
+    polaridade = tb_en.sentiment.polarity
+    return "polaridade: {}".format(polaridade)
+
+@app.route('/cotacao/', methods=['POST'])
+def cotacao(tamanho):
+    dados = request.get_json()
+    dados_input = [dados[col] for col in colunas]
+    preco = modelo.predict([dados_input])
+    return jsonify(preco=preco[0])
+
+app.run(debug=True)
